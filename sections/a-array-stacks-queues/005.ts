0da5fa366688bb7f6c376368ecd384a726fe3270
Luis Ballote <ealjkl@gmail.com>
2023-03-14 15:20:28 -0600

    Solved 5

--- sections/a-array-stacks-queues/005.ts
+++ sections/a-array-stacks-queues/005.ts
@@ -1,30 +1,49 @@
-function rotateMatrix(matrix: number[][]) {}
-
-function rotateElements(level: number, j: number, matrix: number[][]) {
+function rotateElements(level: number, dj: number, matrix: number[][]) {
   const n = matrix.length;
   let tmp;
-  let mem = matrix[level][n - 1 - level];
-  matrix[level][n - 1 - level] = matrix[level][level];
+  let mem = matrix[level + dj][n - 1 - level];
+  matrix[level + dj][n - 1 - level] = matrix[level][level + dj];
 
-  tmp = matrix[n - 1 - level][n - 1 - level];
-  matrix[n - 1 - level][n - 1 - level] = mem;
+  tmp = matrix[n - 1 - level][n - 1 - level - dj];
+  matrix[n - 1 - level][n - 1 - level - dj] = mem;
   mem = tmp;
 
-  tmp = matrix[n - 1 - level][level];
-  matrix[n - 1 - level][level] = mem;
+  tmp = matrix[n - 1 - level - dj][level];
+  matrix[n - 1 - level - dj][level] = mem;
   mem = tmp;
 
-  matrix[level][level] = mem;
+  matrix[level][level + dj] = mem;
 }
 
-const matrix = [
-  [5, 1, 9, 11],
-  [2, 4, 8, 10],
-  [13, 3, 6, 7],
-  [15, 14, 12, 16],
-];
-console.log("1: ", matrix);
+function rotateLevel(level: number, matrix: number[][]) {
+  const n = matrix.length;
+  const m = n - 2 * level;
+  for (let dj = 0; dj < m - 1; dj++) {
+    rotateElements(level, dj, matrix);
+  }
+}
 
-rotateElements(0, 0, matrix);
+function rotateMatrix(matrix: number[][]) {
+  const n = matrix.length;
+  for (let level = 0; level < Math.ceil(n / 2); level++) {
+    rotateLevel(level, matrix);
+  }
+}
 
-console.log("2: ", matrix);
+// const matrix = [
+//   [5, 1, 9, 11],
+//   [2, 4, 8, 10],
+//   [13, 3, 6, 7],
+//   [15, 14, 12, 16],
+// ];
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// console.log("1: ", matrix);
+
+// rotateMatrix(matrix);
+
+// console.log("2: ", matrix);
